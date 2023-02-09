2c106c08ad8b36940c6caa2e8a7b80f464780d60
Dharmendra kumar <dharmendragangwar609@gmail.com>
2023-02-09 12:35:25 +0530

    polymorphism code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,13 +6,13 @@ public class Main {
     {
         public int product(int x, int y)
         {
-            return 0;
+            return (x*y);
         }
         public int product(int x, int y, int z) {
-            return 0;
+            return (x*y*z);
         }
         public double product(double x, double y) {
-            return 0;
+            return (x*y);
         }
 
     }
