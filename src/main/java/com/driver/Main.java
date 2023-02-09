301a54b8170b9924742e8fed07ecdf3bdc784ac1
Dharmendra kumar <dharmendragangwar609@gmail.com>
2023-02-09 10:33:38 +0530

    plymorphism code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,26 @@ package com.driver;
 
 public class Main {
 
+    public static class Product
+    {
+        public int product(int x, int y)
+        {
+            return 0;
+        }
+        public int product(int x, int y, int z) {
+            return 0;
+        }
+        public double product(double x, double y) {
+            return 0;
+        }
+
+    }
+    public static  void main(String[] args )
+    {
+        Product  p=new Product ();
+        p.product(5,6);
+        p.product(4,5,6);
+        p.product(10.2,12.3);
+    }
+
 }
